0c05923ff1628c5f25a13d4c8dcb658993aaca1a
Jan Klimo <jan.klimo@gmail.com>
2020-02-28 09:37:26 +0700

    marker styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css";
 import Map from "./Map";
+import { LoadScript } from "@react-google-maps/api";
 
 function App() {
   return (
     <div className="container">
-      <h1>React Google Maps API - Markers test demo</h1>
-      <Map />
+      <LoadScript
+        id="script-loader"
+        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+      >
+        <h1>React Google Maps API - Markers test demo</h1>
+        <Map />
+      </LoadScript>
     </div>
   );
 }
--- src/Map.tsx
+++ src/Map.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { GoogleMap, Marker } from "@react-google-maps/api";
 
 const mapContainerStyle = {
   height: "30rem",
@@ -14,65 +14,87 @@ const center = {
 const markers = [
   { lat: 40.759941, lng: -73.978669 },
   { lat: 40.766785, lng: -73.9381 },
+  { lat: 40.796785, lng: -73.9481 },
+  { lat: 40.776785, lng: -73.9181 },
+  { lat: 40.739941, lng: -73.918669 },
+  { lat: 40.729941, lng: -73.998669 },
   { lat: 40.809273, lng: -73.997105 }
 ];
 
+const circle = (size: number) => {
+  const radius = size / 2;
+  const anchor = new google.maps.Point(radius, radius);
+
+  return {
+    anchor,
+    path: `M ${radius},${radius} m -${radius},0
+           a ${radius},${radius} 0 1,0 ${radius * 2},0
+           a ${radius},${radius} 0 1,0 -${radius * 2},0 Z`
+  };
+};
+
+const markerOptions = (): google.maps.Symbol => {
+  const iconSize = 26;
+  const { path, anchor } = circle(iconSize);
+  const labelOrigin = new google.maps.Point(iconSize / 2, iconSize / 2);
+
+  return {
+    anchor,
+    fillColor: "#0c2340",
+    fillOpacity: 1,
+    labelOrigin,
+    path,
+    strokeColor: "#e8e8e8",
+    strokeWeight: 1.5
+  };
+};
+
 interface State {
-  zIndex: number;
+  selected: number;
 }
 
 export default class Map extends Component<{}, State> {
   state: Readonly<State> = {
-    zIndex: 1
+    selected: 0
   };
 
   render() {
-    const { zIndex } = this.state;
+    const { selected } = this.state;
 
     return (
-      <LoadScript
-        id="script-loader"
-        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
-      >
-        <div className="map-wrapper">
-          <GoogleMap
-            id="marker-example"
-            mapContainerStyle={mapContainerStyle}
-            zoom={12}
-            center={center}
-            options={{
-              draggable: false
-            }}
-          >
-            {markers.map((marker, index) => (
+      <div className="map-wrapper">
+        <GoogleMap
+          id="marker-example"
+          mapContainerStyle={mapContainerStyle}
+          zoom={12}
+          center={center}
+          options={{
+            draggable: false
+          }}
+        >
+          {markers.map((marker, index) => {
+            const zIndex =
+              index === selected ? markers.length + 2 : markers.length - index;
+
+            return (
               <Marker
                 key={index}
-                label={(index + 1).toString()}
+                label={{
+                  text: (index + 1).toString(),
+                  color: "white",
+                  fontSize: "12px",
+                  fontFamily: "Verdana",
+                  fontWeight: "700"
+                }}
                 position={marker}
                 zIndex={zIndex}
+                onClick={() => this.setState({ selected: index })}
+                icon={markerOptions()}
               />
-            ))}
-          </GoogleMap>
-        </div>
-        <h2>Edit zIndex</h2>
-        <p>
-          Current zIndex: <b>{zIndex}</b>
-        </p>
-        <p>
-          <button
-            className="btn"
-            onClick={() => this.setState({ zIndex: zIndex + 1 })}
-          >
-            +
-          </button>
-          <button
-            className="btn"
-            onClick={() => this.setState({ zIndex: zIndex - 1 })}
-          >
-            -
-          </button>
-        </p>
-      </LoadScript>
+            );
+          })}
+        </GoogleMap>
+      </div>
     );
   }
 }
